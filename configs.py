b1f99e30fa5132bcbef064d252210caecc446a25
Nick Yefremov <e_n_k@list.ru>
2018-10-06 03:14:08 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,4 +3,4 @@ div_class = "rpBJOHq2PR60pnwJlUyP0 s17qbtaw-0 bxapWf"
 
 client = {'id': "EhR0dh5AmeC4LA", 'secret': 
 'ljCDgiqhGF2XmNnEZ19YF0oJZYw', 'username': 'Autumn_Leaf_', 'password': 
-"89/b/3?!MK", 'agent': 'testone'}
+"", 'agent': 'testone'}
