ca86ac26f669df99d1e641ed50a38b8faaa6fb1b
Nick Yefremov <e_n_k@list.ru>
2019-04-27 01:18:32 +0300

    Added Hoare partition method

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -20,7 +20,7 @@ class Quicksort:
         self.array = array
 
     def __call__(self, p, r):
-        q = self.partitionate(p, r)
+        q = self.partition(p, r)
         if r > p:
             self(p, q-1)
             self(q+1, r)
@@ -29,7 +29,7 @@ class Quicksort:
     def swap(self, i, j):
         self.array[i], self.array[j] = self.array[j], self.array[i]
 
-    def partitionate(self, p, r):
+    def partition(self, p, r):
         i = p - 1
         x = self.array[r]
         for j in range(p, r):
@@ -37,8 +37,29 @@ class Quicksort:
                 i += 1
                 self.swap(i, j)
         if p < r:
-            self.swap(i+1, r)
+            self.swap(i+1,   r)
         return i+1
+
+    def hoare_partition(self, p, r):
+        x = self.array[r]
+        i = p
+        j = r
+        while True:
+            while self.array[j] > x:
+                j -= 1
+            while self.array[i] < x:
+                i += 1
+            if i < j:
+                self.swap(i, j)
+            else:
+                return j
+
+    def sort_hoare(self, p, r):
+        q = self.hoare_partition(p, r)
+        if r > p:
+            self(p, q - 1)
+            self(q + 1, r)
+        return self
 
     def __str__(self):
         return "%s\n" % str(self.array)
@@ -48,3 +69,8 @@ qs = Quicksort([14, 7, 12, 1, 0, 8, 6])
 st_time = time.time()
 print(qs(0, 6))
 print("Sorting took %f seconds" % float(time.time() - st_time))
+
+qs2 = Quicksort([14, 7, 12, 1, 0, 8, 6])
+st_time = time.time()
+print(qs.sort_hoare(0, 6))
+print("Sorting with Hoare partition took %f seconds" % float(time.time() - st_time))
